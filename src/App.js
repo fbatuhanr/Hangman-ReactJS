298482a21adf0f56ba22601e260d3d822cf17e22
fbatuhanr <fbatuhanr@gmail.com>
2021-09-21 00:33:15 +0300

    axios and bootstrap installed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function App() {
+
+  const [countries, setCountries] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    
+    axios.get('https://restcountries.eu/rest/v2/all')
+    .then(function (response) {
+      // handle success
+      setCountries(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      setError(error);
+    })
+    .then(function () {
+      // always executed
+    });
+
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+      {
+        countries && countries.map(country => 
+          <div className="col col-3">
+            <div className="mt-1 mb-1 border">
+            {country.name}
+            </div>
+          </div>
+        )
+      } 
+      </div>
     </div>
   );
 }
